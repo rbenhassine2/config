0d669d28b536faf776800d0fe160cb0ca269eae2
Raouf Ben Hassine <raouf.ben.hassine.89@gmail.com>
2025-11-15 19:27:51 -0500

    checkpoint 2025-11-16T00:27+00:00

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
--- Disable inlay hints by default to prevent errors
-vim.g.lazyvim_python_lsp = "pyrefly"
-vim.g.lazyvim_python_ruff = "ruff"
