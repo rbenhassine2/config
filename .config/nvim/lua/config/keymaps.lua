bc2d20026c1c2d41434b99e301c8ca918b58a056
Raouf Ben Hassine <raouf.ben.hassine.89@gmail.com>
2025-11-23 22:19:48 -0500

    disable window resizing keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,12 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<A-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-vim.keymap.set("n", "<A-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-vim.keymap.set("n", "<A-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-vim.keymap.set("n", "<A-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
-
-vim.keymap.set("n", "<A-S-k>", "<cmd>resize +10<cr>", { desc = "Increase Window Height" })
-vim.keymap.set("n", "<A-S-j>", "<cmd>resize -10<cr>", { desc = "Decrease Window Height" })
-vim.keymap.set("n", "<A-S-h>", "<cmd>vertical resize -10<cr>", { desc = "Decrease Window Width" })
-vim.keymap.set("n", "<A-S-l>", "<cmd>vertical resize +10<cr>", { desc = "Increase Window Width" })
+-- vim.keymap.set("n", "<A-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+-- vim.keymap.set("n", "<A-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+-- vim.keymap.set("n", "<A-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+-- vim.keymap.set("n", "<A-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+--
+-- vim.keymap.set("n", "<A-S-k>", "<cmd>resize +10<cr>", { desc = "Increase Window Height" })
+-- vim.keymap.set("n", "<A-S-j>", "<cmd>resize -10<cr>", { desc = "Decrease Window Height" })
+-- vim.keymap.set("n", "<A-S-h>", "<cmd>vertical resize -10<cr>", { desc = "Decrease Window Width" })
+-- vim.keymap.set("n", "<A-S-l>", "<cmd>vertical resize +10<cr>", { desc = "Increase Window Width" })
