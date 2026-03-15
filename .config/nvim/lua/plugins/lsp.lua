0ba2d79bbb5f922ab19f45fba23816ce6886de0c
Raouf Ben Hassine <raouf.ben.hassine.89@gmail.com>
2026-03-15 08:15:43 -0400

    checkpoint 2026-03-15T12:15+00:00

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,29 +3,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        clangd = {
-          cmd = {
-            "clangd",
-            "--offset-encoding=utf-16",
-            "--background-index",
-            "--clang-tidy",
-            "--clang-tidy-checks=*",
-            "--completion-style=detailed",
-            "--function-arg-placeholders",
-            "--header-insertion=iwyu",
-            "--all-scopes-completion",
-            "--query-driver=/usr/bin/**/clang*",
-          },
-          init_options = {
-            clangdFileStatus = true,
-            usePlaceholders = true,
-            completeUnimported = true,
-          },
-          filetypes = { "c", "cpp", "objc", "objcpp" },
-          extra_args = {
-            "--query-driver=/usr/bin/**/clang*",
-          },
-        },
+
         -- pyright = {
         --   python = {
         --     pythonPath = "python",
@@ -56,10 +34,6 @@ return {
         },
         ty = false,
         ruff_lsp = false,
-        erlang_ls = false,
-        erlangls = false,
-        rubocop = false,
-        ruby_lsp = false,
         fsautocomplete = false,
       },
     },
