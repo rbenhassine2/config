62fbd971fd245035df98d0f023228d5e962127dc
Raouf Ben Hassine <raouf.ben.hassine.89@gmail.com>
2026-06-02 19:10:26 -0400

    checkpoint 2026-06-02T23:10+00:00

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,1 +1,2 @@
-return { { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-frappe" } } }
+-- return { { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-frappe" } } }
+return { { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 } }
