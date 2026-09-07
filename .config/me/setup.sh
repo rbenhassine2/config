7cee308ffed69738724d4e92e3e89df291ee9ef8
Raouf Ben Hassine <raouf.benhassine@proton.me>
2026-09-07 03:10:03 -0400

    checkpoint 2026-09-07T07:10+00:00

--- .config/me/setup.sh
+++ .config/me/setup.sh
@@ -20,7 +20,7 @@ sudo apt update
 sudo apt upgrade -y
 
 # install dependencies
-sudo apt install jq git curl wget gawk build-essential ca-certificates btop ffmpeg zstd dunst polybar autorandr rofi picom i3lock feh copyq redshift blueman -y
+sudo apt install i3 jq git curl wget gawk build-essential ca-certificates btop ffmpeg zstd dunst polybar autorandr rofi picom i3lock feh copyq redshift blueman -y
 
 # download and setup config.
 # Copied from https://www.atlassian.com/git/tutorials/dotfiles
@@ -111,6 +111,22 @@ curl -fsSL https://opencode.ai/install | bash
 
 # Install ollama
 curl -fsSL https://ollama.com/install.sh | sh
+
+# Install gh (GitHub CLI)
+# Copied from https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
+# configure git
+git config --global user.name "Raouf Ben Hassine"
+git config --global user.email "raouf.benhassine@proton.me"
 
 # Install incus
 if $install_incus; then
